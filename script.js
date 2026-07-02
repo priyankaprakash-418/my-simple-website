c2617b03a3e7bf345bc1858a3e83b4a7e2ef67c3
Priyanka Prakash <priyanka.prakash@frugalscientific.com>
2026-07-03 01:55:25 +0400

    Initial website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickBtn").addEventListener("click", function () {
+    document.getElementById("message").textContent =
+        "Your CI/CD pipeline is working perfectly!";
+});
